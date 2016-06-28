fa0d8c27d8fb730414539292881c5eafd708c2b0
Ryan Evans <ryan@Ryans-MacBook-Pro.local>
2016-06-28 13:29:59 -0500

    added deleteVM function

--- cloudModules/cloudCompute.py
+++ cloudModules/cloudCompute.py
@@ -6,7 +6,7 @@ def bootVM(token_id):
     url2 = globalVars.computeURL.format(globalVars.tenant_id) #Replaces {0} from config file with the appropriate tenant id
 
     body = {"server": 
-                {"name": "server-test",
+                {"name": "server-test1",
                  "imageRef": "eec1fa2e-a8ba-4725-ab6c-2c65acb958fc",
                  "flavorRef": "1"}}
 
@@ -15,4 +15,12 @@ def bootVM(token_id):
     json_body = json.dumps(body)
 
     r = requests.post(url2, json_body, headers=my_headers)
+    print r
+
+def deleteVM(token_id, server_id):
+    url = globalVars.computeURL.format(globalVars.tenant_id) + '/' + server_id
+
+    my_headers = {"X-Auth-Token": token_id}
+
+    r = requests.delete(url, headers=my_headers)
     print r
