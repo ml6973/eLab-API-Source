3a48d72e0c9fcfed78d142a02c28f28829d0e400
gregoryhooks <gregoryhooksjr@gmail.com>
2016-11-18 10:46:06 -0600

    Added unique name constraint for instances

--- api/models.py
+++ api/models.py
@@ -38,7 +38,7 @@ class Image(models.Model):
 class Instance(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey('Image', on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    name = models.CharField(unique=True, max_length=50)
     cloud = models.ForeignKey('Cloud')
     computeId = models.CharField(max_length=100)
     ipaddr = models.CharField(max_length=20)
